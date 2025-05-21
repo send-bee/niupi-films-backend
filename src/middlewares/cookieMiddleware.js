22be45560b129a6340e0906ff152030e88cf190c
Send Bee <betamotoe5@gmail.com>
2025-05-21 15:46:56 -0500

    testing

--- src/middlewares/cookieMiddleware.js
+++ src/middlewares/cookieMiddleware.js
@@ -1,7 +1,8 @@
+import jwt from "jsonwebtoken"
+
 const jwtMiddleware = (req, res, next) => {
     try {
         const token = req.cookies.access_token;
-        console.log(req.cookies)
         const result = jwt.verify(token, process.env.SECRET_JWT_KEY);
         console.log(result)
         if (result) {
